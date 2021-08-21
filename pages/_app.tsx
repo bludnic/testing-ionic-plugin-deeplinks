a5c85031d6656aa808ab72a3a981a764de00b621
bludnic <victorcpla@gmail.com>
2021-08-21 15:04:12 +0100

    feat: add Material-UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import React, { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+import { theme } from 'theme/light';
+
+export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles && jssStyles.parentElement) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Админка доставлятора</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </React.Fragment>
+    );
 }
-export default MyApp;
